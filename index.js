b531fbc6078bedf95626cd4f57d6f61e3357bf61
Harrison Weiss <weiss.harrison@gmail.com>
2017-10-02 23:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 
-function nestTarget(){
+function nestedTarget(){
   return document.getElementById('nested').querySelectorAll('target')
 }
